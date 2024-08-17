2b0545c4a6ccb52bc4964f0ed84fdd7cbb2d425c
Kamalov-Q <kamolovquvomiddin9@gmail.com>
2024-08-17 21:09:55 +0500

    New changes

--- src/Pages/Cars/Cars.jsx
+++ src/Pages/Cars/Cars.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable no-unused-vars */
 import { DeleteFilled, EditFilled, PlusCircleFilled } from "@ant-design/icons";
 import "./Cars.css";
